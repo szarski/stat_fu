7f4a815c5d0480cdf603bb0709a5d71d0d591d72
Jacek Szarski <jacek@applicake.com>
2010-08-18 11:44:45 -0500

    added the Array#each_cmbination extenstion

--- lib/stat_fu.rb
+++ lib/stat_fu.rb
@@ -1,1 +1,13 @@
-# StatFu
+Hash.class_eval do
+  def each_combination(&block)
+    blocks = [block]
+    self.to_a.select {|k,v| v.is_a?(Array)}.each_with_index do |(param_name, values), block_index|
+      blocks << lambda do |params|
+        values.each do |value|
+          blocks[block_index].call(params.merge({param_name => value}))
+        end
+      end
+    end
+    blocks.last.call(self.reject {|k,v| v.is_a?(Array)})
+  end
+end
