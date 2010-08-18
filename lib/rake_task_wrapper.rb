249b3b49399ef04242c462e537cda405efbe9fa5
Jacek Szarski <jacek@applicake.com>
2010-08-18 12:07:15 -0500

    created default_update and default_clear rake tasks methods

--- lib/rake_task_wrapper.rb
+++ lib/rake_task_wrapper.rb
@@ -14,4 +14,25 @@ class Statistic::RakeTaskWrapper
     specification = Statistic::RakeTaskSpecification.new(task_name, :description => description, :block => code_block, :namespaces => self.namespaces, :klass_name => klass.name)
     Statistic.add_rake_task specification
   end
+
+  def default_update(method_name, description, params_spec)
+    self.send method_name, description do
+      params_spec = params_spec.inject({}) {|sum, (k,v)| v=v.is_a?(Proc) ? v.call : v ;sum.merge({k=>v})}
+      params_spec.each_combination do |params|
+        puts "\r calculating #{klass.name} for: #{params.inspect}"
+        klass.create_or_update params
+      end
+    end
+  end
+
+  def default_clear(method_name, description)
+    self.send method_name, description do
+      klass.delete_all
+      if klass.count == 0
+        puts "deleted all"
+      else
+        puts "ERROR!"
+      end
+    end
+  end
 end
