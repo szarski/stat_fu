462eb4b427894860c57f81c3d7a217e69d3a4b2e
Jacek Szarski <jacek@applicake.com>
2010-09-11 13:03:46 -0500

    RakeTasksWrapper fixes

--- lib/rake_task_wrapper.rb
+++ lib/rake_task_wrapper.rb
@@ -1,21 +1,22 @@
 class Statistic::RakeTaskWrapper
   attr_accessor :klass, :namespaces
   def initialize(klass, _namespaces=nil)
-    self.klass = klass
-    self.namespaces = _namespaces || [] #[klass.name.underscore.gsub(/^.*\//,'').to_sym]
+    @klass = klass
+    @namespaces = _namespaces || [] #[klass.name.underscore.gsub(/^.*\//,'').to_sym]
   end
 
   def namespace(name)
-    yield self.class.new(self.klass, self.namespaces + [name])
+    yield self.class.new(@klass, @namespaces + [name])
   end
 
   def method_missing(task_name, description=nil)
     code_block = lambda{yield}
-    specification = Statistic::RakeTaskSpecification.new(task_name, :description => description, :block => code_block, :namespaces => self.namespaces, :klass_name => klass.name)
+    specification = Statistic::RakeTaskSpecification.new(task_name, :description => description, :block => code_block, :namespaces => @namespaces, :klass_name => @klass.name)
     Statistic.add_rake_task specification
   end
 
   def default_update(method_name, description, params_spec)
+    klass = @klass
     self.send method_name, description do
       params_spec = params_spec.inject({}) {|sum, (k,v)| v=v.is_a?(Proc) ? v.call : v ;sum.merge({k=>v})}
       klass.batch(params_spec).fill_up {|stat| puts "  #{stat.coherent ? '+' : '!'}  #{stat.class} for: #{stat.parameters}"}
@@ -23,6 +24,7 @@ class Statistic::RakeTaskWrapper
   end
 
   def default_clear(method_name, description=nil)
+    klass = @klass
     self.send method_name, description do
       klass.delete_all
       if klass.count == 0
