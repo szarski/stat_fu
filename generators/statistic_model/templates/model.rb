efa4ad7422e0775e2e943d1680c41db6ecde3af0
Jacek Szarski <jacek@applicake.com>
2010-09-13 15:35:24 -0500

    model template updates

--- generators/statistic_model/templates/model.rb
+++ generators/statistic_model/templates/model.rb
@@ -1,65 +1,84 @@
 class Statistic::<%= class_name %> < Statistic::Base
   set_table_name "statistic_<%= plural_name %>"
 
-  parameters # place here fields that are parameters for the stat, like:
-  # parameters :date, :user_id
+  # # Place fields that are your input here, like:
+  # # parameters :date, :user_id, :category, :optional => [:category]
+  parameters <%= attributes.collect{|a| ":#{a.name}"}.join(', ') %>
 
   def count
 
-    # put your code that counts stats here
+    # # Put your code that calculates one single record's data here.
+    # # The function should set all the output variables.
+    # # Example:
+    # #
+    # # if (self.category)
+    # #   self.projects_count = User.find(self.user_id).projects.with_categeory_name(category).count
+    # # else
+    # #   self.projects_count = User.find(self.user_id).projects.count
+    # # end
+    # #
+    # # You can cache stuff between records:
+    # #
+    # # self.projects_count = self.cache_in_batch "projects_for_user_#{self.user_id}" do
+    # #   User.find(self.user_id).projects.with_categeory_name(category).count
+    # # end
 
   end
 
   def check
 
-    # put your code that validates data coherence here
+    # # Put your code that validates single record's data coherence here.
+    # # The code has to return true or false.
 
     return true
   end
 
-# This method is optional. When doing an update, if it is specified, Stat_fu will call it
-# and update only if up_to_date?() returns false
-#
+
+
+# # This method is optional. Record won't be updated unless up_to_date? returns false or is not specified:
+# #
 #  def up_to_date?
 #
 #  end
 
-#  rake_tasks do |r|
-#    # put your rake tasks code here.
-#    r.namespace :<%= plural_name %> do |n|
-#      n.namespace :update do |u|
-#        u.all "update all stats for <%= plural_name %>" do
-#          [].each do |params|
-#            Statistic::<%= class_name %>.create_or_update params
-#          end
-#        end
-#        u.namespace :all do
-#          a.force "update all stats for <%= plural_name %>, force up_to_date ones" do
-#            [].each do |params|
-#               Statistic::<%= class_name %>.create_or_update params.merge(:force => true)
-#            end
-#          end
-#        end
-#      end
-#      n.namespace :clear do |f|
-#        f.all "delete all <%= plural_name %> stats!" do
-#          Statistic::<%= class_name %>.destroy_all
-#          if Statistic::<%= class_name %>.count == 0
-#            puts "deleted all"
-#          else
-#            puts "There still are some stats left!"
-#          end
-#        end
-#      end
-#
-#
-#    end
-#  #
-#  # first one produces:
-#  # rake statistics:<%= plural_name %>:update:all
-#  # second one:
-#  # rake statistics:<%= plural_name %>:update:all:force
-#  #
-#  end
+
+
+# # You can define rake tasks here:
+# #
+# # Put the default argument set you want to update:
+#   UPDATE_HASH = {}
+# # like: 
+# # UPDATE_HASH = {:date => lambda{:date => (3.months.ago.to_date..Time.now.to_date).to_a}, :user_id => User.all.map(&:id)}
+# 
+#   rake_tasks do |r|
+#     r.namespace :<%= plural_name %> do |n|
+#       n.namespace :update do |u|
+#         u.default_update :all, "update all stats for <%= plural_name %>", UPDATE_HASH
+#         u.namespace :all do |a|
+#           a.default_update :force, "update all stats for <%= plural_name %>, force up_to_date ones", UPDATE_HASH.merge{:force => true}
+#         end
+#       end
+#       n.namespace :clear do |c|
+#         c.default_clear :all, "clear all <%= plural_name %> stats"
+#       end
+#       n.custom_task "Some Custom Task" do
+#         # code here
+#       end
+#     end
+#   end
+# 
+# # These produce:
+# #
+# # rake statistics:<%= plural_name %>:update:all
+# #
+# # rake statistics:<%= plural_name %>:update:all:force
+# #
+# # rake statistics:<%= plural_name %>:clear:all
+# #
+# # rake statistics:<%= plural_name %>:custom_task
+# #
+# # check rake stats:help to see all the stats tasks.
+
+  describe_output <%= attributes.collect{|a| ":#{a.name} => :sum"}.join(', ') %>, :custom_method => lambda{|records| records.count}
 
 end
